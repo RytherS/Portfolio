5cd4b247bd9daa3a9dd9c43e0d38b9d2c5b47f71
Sebastian Ryther <72417924+RytherS@users.noreply.github.com>
2023-02-21 22:46:35 -0600

    Added basic lazy module loading

---
+++ src/app/modules/index.ts
@@ -0,0 +1,4 @@
+export { ContactModule } from './contact/contact.module';
+export { HeaderModule } from './header/header.module';
+export { HomeModule } from './home/home.module';
+export { PortfolioModule } from './portfolio/portfolio.module';
