646a4099ef0e9cc53bfea09fc7980ef312b9beac
Sebastian Ryther <72417924+RytherS@users.noreply.github.com>
2023-04-15 15:24:30 -0500

    Created ProjectInfoModule and component stub

--- src/app/modules/index.ts
+++ src/app/modules/index.ts
@@ -1,4 +1,6 @@
+export { AboutModule } from './about/about.module';
 export { ContactModule } from './contact/contact.module';
 export { HeaderModule } from './header/header.module';
 export { HomeModule } from './home/home.module';
 export { PortfolioModule } from './portfolio/portfolio.module';
+export { ProjectInfoModule } from './project-info/project-info.module';
