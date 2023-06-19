ebc77fd671eae17f17b3ece027af490d4c1e8cec
Sebastian Ryther <72417924+RytherS@users.noreply.github.com>
2023-06-18 21:26:18 -0500

    Some super small cleanup

--- src/app/modules/home/home.module.ts
+++ src/app/modules/home/home.module.ts
@@ -1,8 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { LoadingSpinnerModule } from '../shared/loading-spinner/loading-spinner.module';
-
 import { HomeRoutingModule } from './home-routing.module';
 import { HomeComponent } from './components';
 
@@ -13,8 +11,7 @@ import { HomeComponent } from './components';
   ],
   imports: [
     CommonModule,
-    HomeRoutingModule,
-    LoadingSpinnerModule
+    HomeRoutingModule
   ]
 })
 export class HomeModule { }
