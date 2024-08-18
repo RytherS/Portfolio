333a396b34807f17e25778cceab0ac22afb57348
Sebastian Ryther <72417924+RytherS@users.noreply.github.com>
2024-08-18 15:46:40 -0500

    Setup for active tab styling, some linting cleanup

--- src/app/modules/shared/components/header/header.component.ts
+++ src/app/modules/shared/components/header/header.component.ts
@@ -1,29 +1,30 @@
-import { Component, inject } from '@angular/core';
-import { Router } from '@angular/router';
+import { Component, inject } from "@angular/core";
+import { Router } from "@angular/router";
 
 
 @Component({
-  selector: 'sr-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+	selector: "sr-header",
+	templateUrl: "./header.component.html",
+	styleUrls: ["./header.component.scss"]
 })
 export class HeaderComponent {
-  private router: Router = inject(Router)
+	private router: Router = inject(Router);
 
-  public readonly bracketString = '<>'
+	public readonly bracketString = "<>";
 
-  public readonly navRouteMappings: Map<string, string> = new Map<string, string>([
-    [ '/home', 'Home' ],
-    [ '/portfolio', 'Portfolio' ],
-    [ '/contact', 'Contact' ],
-    [ '/about', 'About' ],
-  ]);
+	public readonly navRouteMappings: Map<string, string> = new Map<string, string>([
+		[ "/home", "Home" ],
+		[ "/portfolio", "Portfolio" ],
+		[ "/contact", "Contact" ],
+		[ "/about", "About" ],
+	]);
 
-  public onNavTabClicked(route: string): void {
-    this.router.navigate([route])
-  }
+	public onNavTabClicked(route: string): void {
+		this.router.navigate([route]);
+	}
 
-  public originalOrder(a: any, b: any): number {
-    return 0;
-  }
+  // Sorting function for header links to remain in the order they are defined in navRouteMappings up above
+	public originalOrder(): number {
+		return 0;
+	}
 }
