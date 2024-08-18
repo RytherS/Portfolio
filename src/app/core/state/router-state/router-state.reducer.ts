80b7709dd8064cd0f0bd5cd13b2e044e1a1521cb
Sebastian Ryther <72417924+RytherS@users.noreply.github.com>
2024-08-18 18:52:58 -0500

    SR21--Added RouterState

---
+++ src/app/core/state/router-state/router-state.reducer.ts
@@ -0,0 +1,7 @@
+import { routerReducer } from "@ngrx/router-store";
+import { ActionReducerMap } from "@ngrx/store";
+import { RouterState } from "./router-state.state";
+
+export const routerStateReducer: ActionReducerMap<RouterState> = {
+	routerState: routerReducer,
+};
